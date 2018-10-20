d9cd479bfe555bd2f13139877874283327e3bebc
Ben <benthompson4496@gmail.com>
2018-10-20 17:31:49 -0500

    assignment work for schekpoint 2 of bloccit

--- spec/integration/statis_spec.js
+++ spec/integration/statis_spec.js
@@ -5,16 +5,20 @@ const base = "http://localhost:3000/";
 describe("routes : static", () => {
 
     describe("GET /", () => {
-
-
         it("should return status code 200", (done) => {
-
             request.get(base, (err, res, body) => {
                 expect(res.statusCode).toBe(200);
-
                 done();
             });
         });
+    });
 
+    describe("GET /marco", () => {
+        it("should return 'polo'", (done) => {
+            request.get(base, (err, res, body) => {
+                expect(res.statusCode).toBe(200);
+                done();
+            });
+        });
     });
 });
--- src/app.js
+++ src/app.js
@@ -2,7 +2,10 @@ const express = require("express");
 const app = express();
 
 const routeConfig = require("./config/route-config.js");
-
 routeConfig.init(app);
+
+// app.use("/marco", (req, res, next) => {
+//     res.send("polo")
+// });
 
 module.exports = app;
