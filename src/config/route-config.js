06556e6d62efe851bc8c34d24fb2561349ef4f06
Ben <benthompson4496@gmail.com>
2018-11-29 06:35:36 -0600

    a lot of files - sign in, sign out, completing checkpoint 11 work

--- src/config/route-config.js
+++ src/config/route-config.js
@@ -5,8 +5,11 @@ module.exports = {
         const staticRoutes = require("../routes/static");
         const postRoutes = require("../routes/posts");
         const topicRoutes = require("../routes/topics");
+        const userRoutes = require("../routes/users");
+
         app.use(staticRoutes);
         app.use(postRoutes);
         app.use(topicRoutes);
+        app.use(userRoutes);
     }
 }
