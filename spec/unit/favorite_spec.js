2e64f64cf9356232377de5c27befe4ab63ddd333
Ben <benthompson4496@gmail.com>
2018-12-07 11:25:03 -0600

    implementing favoriting - all tests passing

--- spec/unit/favorite_spec.js
+++ spec/unit/favorite_spec.js
@@ -61,6 +61,89 @@ describe("Favorite", () => {
         });
     });
 
- // tests here
+    //tests
+    describe("#create()", () => {
+        it("should create a favorite for a post on a user", (done) => {
+            Favorite.create({
+                postId: this.post.id,
+                userId: this.user.id
+            })
+            .then((favorite) => {
+                expect(favorite.postId).toBe(this.post.id);
+                expect(favorite.userId).toBe(this.user.id);
+                done();
+       
+            })
+            .catch((err) => {
+                console.log(err);
+                done();
+            });
+        });
+        it("should not create a favorite without assigned post or user", (done) => {
+            Favorite.create({
+                userId: null
+            })
+            .then((favorite) => {
+                done();
+            })
+            .catch((err) => {
+                expect(err.message).toContain("Favorite.userId cannot be null");
+                expect(err.message).toContain("Favorite.postId cannot be null");
+                done();
+            })
+        }); 
+    });
+
+    describe("#setUser()", () => {
+        it("should associate a favorite and a user together", (done) => {
+            Favorite.create({           // create a favorite on behalf of this.user
+                postId: this.post.id,
+                userId: this.user.id
+            })
+            .then((favorite) => {
+                this.favorite = favorite;     // store it
+                expect(favorite.userId).toBe(this.user.id); //confirm it was created for this.user
+    
+                User.create({                 // create a new user
+                    email: "bob@example.com",
+                    password: "password"
+                })
+                .then((newUser) => {
+                    this.favorite.setUser(newUser)  // change the favorite's user reference for newUser
+                .then((favorite) => {
+                    expect(favorite.userId).toBe(newUser.id); //confirm it was updated
+                    done();
+                });
+                })
+                .catch((err) => {
+                    console.log(err);
+                    done();
+                });
+            })
+        });
+    });
+   
+    describe("#getUser()", () => {
+        it("should return the associated user", (done) => {
+            Favorite.create({
+                userId: this.user.id,
+                postId: this.post.id
+            })
+            .then((favorite) => {
+                favorite.getUser()
+                .then((user) => {
+                    expect(user.id).toBe(this.user.id); // ensure the right user is returned
+                    done();
+                })
+            })
+            .catch((err) => {
+                console.log(err);
+                done();
+            });
+        });
+    });
+
+
+
 
 });
