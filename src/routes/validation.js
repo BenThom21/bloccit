a10f30a36d0f092b70932c728e6a03e540bd809b
Ben <benthompson4496@gmail.com>
2019-01-21 19:48:50 -0600

    changes

--- src/routes/validation.js
+++ src/routes/validation.js
@@ -11,6 +11,7 @@ module.exports = {
         const errors = req.validationErrors();
 
         if (errors) {
+            console.log(errors);
             req.flash("error", errors);
             return res.redirect(303, req.headers.referer)
         } else {
